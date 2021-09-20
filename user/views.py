4957b06032ba7f464d14809d848099f22c5c2f6a
siwon <siwon8783@gmail.com>
2021-09-20 16:15:29 +0900

    로그인 유효성 검증

--- user/views.py
+++ user/views.py
@@ -2,27 +2,19 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.hashers import make_password, check_password
 from user.models import User
+from .forms import LoginForm
 
 
 def login(request):
-    if request.method == 'GET':
-        return render(request, 'login.html')
-    elif request.method == 'POST':
-        username = request.POST.get('username', None)
-        password = request.POST.get('password', None)
-
-        res_data = {}
-        if not (username and password):
-            res_data['error'] = '모든 값을 입력해야 합니다.'
-        else:
-            user = User.objects.get(username=username)
-            if check_password(password, user.password):
-                request.session['user'] = user.id
-                return redirect('/')
-            else:
-                res_data['error'] = '비밀번호를 틀렸습니다.'
-
-        return render(request, 'login.html', res_data)
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            request.session['user'] = form.user_id
+            # session
+            return redirect('/')
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
 
 
 def register(request):
